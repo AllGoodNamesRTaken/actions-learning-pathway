cb42adeb4a76c9a06a664bebe28eced14e6c0630
AllGoodNamesRTaken <cloudcoursesjoseramon@gmail.com>
2024-11-17 19:38:18 +0100

    Updating branch to look like main

--- go-tests/main.go
+++ go-tests/main.go
@@ -8,10 +8,17 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// main function starts the server
 func main() {
+	// Setup the application and run it
+	e := setupServer()
+	httpPort := getPort()
+	e.Logger.Fatal(e.Start(":" + httpPort))
+}
 
+// setupServer initializes the Echo server with routes
+func setupServer() *echo.Echo {
 	e := echo.New()
-
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
@@ -23,16 +30,19 @@ func main() {
 		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
 	})
 
+	return e
+}
+
+// getPort retrieves the port from environment variables or defaults to 8080
+func getPort() string {
 	httpPort := os.Getenv("PORT")
 	if httpPort == "" {
 		httpPort = "8080"
 	}
-
-	e.Logger.Fatal(e.Start(":" + httpPort))
+	return httpPort
 }
 
-// Simple implementation of an integer minimum
-// Adapted from: https://gobyexample.com/testing-and-benchmarking
+// IntMin returns the minimum of two integers
 func IntMin(a, b int) int {
 	if a < b {
 		return a
